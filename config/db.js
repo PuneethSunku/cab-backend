daae4f9c0f309a515110cf4ff70091802d2efbfc
PuneethSunku <134520958+PuneethSunku@users.noreply.github.com>
2024-11-21 10:07:20 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/smartcab');
+    await mongoose.connect('mongodb+srv://puneethkumarsunku2004:SunkU%402004@cab.zgzjy.mongodb.net/?retryWrites=true&w=majority');
     console.log('MongoDB Connected');
   } catch (error) {
     console.error('MongoDB Connection Error:', error.message);
